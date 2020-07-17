e0ea78bec152d7d7a3b634c2a8f28d0e6379e80e
zoedevelop <58269473+zoedevelop@users.noreply.github.com>
2020-07-17 12:55:22 -0500

    Add files via upload

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,4 +1,4 @@
-/ NOTE! The steps in this file are basically identical to the ones you
+// NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
